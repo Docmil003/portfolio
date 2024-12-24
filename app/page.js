ec5fcb5ce701d8f0f2049b2872df29a2294c76dd
Shyne Sogorb <shyne2003@gmail.com>
2024-12-24 14:17:34 +0100

    Updates

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Home from "../pages/home"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../globals.css'
+
+
+export default function Aplicacion() {
+  return (
+    <div className="Aplicacion">
+      <Home></Home>
+    </div>
+  )
+}
